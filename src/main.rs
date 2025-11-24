bab57d75bd7d2acf762eba7c1c9e9f3345f08b35
0xRektified <bugislife@proton.me>
2025-11-24 15:52:38 +0800

    feat: improve main to use config structure

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,43 @@
 use solana_client::rpc_client::RpcClient;
-use std::env;
+use std::{env, thread::sleep, time::{self}};
 use dotenv::dotenv;
 use anyhow::Result;
 
-fn main() -> Result<()> {
-    dotenv().ok();
+struct Config {
+    rpc_url: String,
+    polling_interval: u64,
+    target_account: String,
+}
 
-    let rpc_url = env::var("RPC_URL")
-        .expect("RPC_URL must be set in .env file");
-    let client = RpcClient::new(rpc_url);
+impl Config {
+    fn from_env() -> Result<(Config)> {
+        dotenv().ok();
 
-    match client.get_latest_blockhash() {
-        Ok(blockhash) => println!("Connected: {}", blockhash),
-        Err(e) => println!("failed: {}", e),
+        Ok(Config {
+            rpc_url: env::var("RPC_URL")
+            .expect("RPC_URL must be set in .env file"),
+
+            polling_interval: env::var("POLL_INTERVAL_SECS")
+            .unwrap_or_else(|_| "10".to_string())
+            .parse()
+            .expect("POLL_INTERVAL_SECS should be a number"),
+        
+            target_account: env::var("TARGET_ACCOUNT")
+                .expect("TARGET_ACCOUNT must be set in .env file"),
+        })
     }
-    println!("Hello, world!");
-    Ok(())
+}
+
+fn main() -> Result<()> {
+
+    let config = Config::from_env()?;
+    let client = RpcClient::new(config.rpc_url);
+    let interval_duration = time::Duration::from_secs(config.polling_interval);
+    loop {
+        sleep(interval_duration);
+        match client.get_latest_blockhash() {
+            Ok(blockhash) => println!("Connected: {}", blockhash),
+            Err(e) => println!("failed: {}", e),
+        }
+    }
 }
